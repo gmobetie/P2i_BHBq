369da7d9c1c47cf80f055c5650a63e92ff70467d
Gabin Mobétie <gmobetie@ensc.fr>
2024-02-09 00:43:08 +0100

    client structure

--- BHBq/Migrations/20240208232044_Clients.Designer.cs
+++ BHBq/Migrations/20240208232044_Clients.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -9,9 +10,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BHBq.Migrations
 {
     [DbContext(typeof(BHBqContext))]
-    partial class BHBqContextModelSnapshot : ModelSnapshot
+    [Migration("20240208232044_Clients")]
+    partial class Clients
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder.HasAnnotation("ProductVersion", "6.0.0");
---
+++ BHBq/Migrations/20240208232044_Clients.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BHBq.Migrations
+{
+    public partial class Clients : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nom = table.Column<string>(type: "TEXT", nullable: false),
+                    Adresse = table.Column<string>(type: "TEXT", nullable: false),
+                    Particulier = table.Column<bool>(type: "INTEGER", nullable: false),
+                    Siret = table.Column<string>(type: "TEXT", nullable: true),
+                    TvaIntracom = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
